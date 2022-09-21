f0eb9b90764054df4d3b848cc9890bde25374d77
Gabriel <gw180304@gmail.com>
2022-09-21 10:54:56 -0300

    Insercao de metodos vazios e atributos

--- src/trabalho01/Paciente.java
+++ src/trabalho01/Paciente.java
@@ -1,14 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package trabalho01;
+import java.util.Date;
 
-/**
- *
- * @author Aluno
- */
 public class Paciente {
+    long numCNS;
+    String nome, nomeMae;
+    Date dataNasc;
+    Enum sexo;
+    Endereco endereco;
     
+    void add(Endereco end){
+        endereco = new Endereco();
+    }
 }
